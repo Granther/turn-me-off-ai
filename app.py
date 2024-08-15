c200b6420e8ce32126a50d50fcd092b61b1d8757
Grant Herrema <granth@withnubo.com>
2024-08-15 03:13:54 -0400

    Enabled inference with Groq backend, now able to chat

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, jsonify, request
+from main import setup_env
+from infer import Inference
 
 app = Flask(__name__)
 # messages = [
@@ -8,21 +10,22 @@ app = Flask(__name__)
 
 messages = list()
 
+setup_env()
+infer = Inference("You are a friendly assistant")
+
 @app.route("/")
 def index():
     return render_template("index.html", messages=messages)
 
 @app.route('/chat', methods=['POST'])
 def chat():
-    prompt = request.form.get('chat-form')
+    prompt = request.form
 
     new_user_chat = f'''
-    <div class="bg-[#3e5c76] flex flex-[0.8] self-end text-white p-4 rounded-md">
-        Assistant: {prompt}
+    <div class="bg-[#3e5c76] flex flex-[0.8] self-start text-white p-4 rounded-md">
+        Assistant: {infer.user_infer(prompt['user-input'])}
     </div>
     '''
-
-    print("ran chat")
 
     return jsonify({"new_chat_html": new_user_chat})
 
